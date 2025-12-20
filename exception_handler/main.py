94592d8382f3b80753a5eaab16ac2874d1a66d29
salswl215 <lyn7666@gmail.com>
2025-12-20 20:44:41 +0900

    FastAPI : Exception handling

---
+++ exception_handler/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI, HTTPException, Request, status
+from starlette.exceptions import HTTPException as StarletteHTTPException
+from fastapi.exceptions import RequestValidationError
+from fastapi.staticfiles import StaticFiles
+from routes import blog
+from utils.common import lifespan
+from utils import exc_handler
+
+app = FastAPI(lifespan=lifespan)
+
+app.mount("/static", StaticFiles(directory = "static"), name = "static")
+app.include_router(blog.router)
+
+app.add_exception_handler(StarletteHTTPException, exc_handler.custom_http_exception_handler)
+app.add_exception_handler(RequestValidationError, exc_handler.validation_exception_handler)
